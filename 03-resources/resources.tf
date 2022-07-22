59d9ff23794f3305814c301b71540ec490338377
Siny <sinydaniel93@gmail.com>
2022-07-22 18:09:50 -0400

    resources

--- 03-resources/resources.tf
+++ 03-resources/resources.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "sample" {
   ami                    = "ami-02cee382129c66e70"
   instance_type          = "t3.micro"
-  vpc_security_group_ids = ["aws_security_group.allow_tls.id"]
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
 }
 
 output "out" {
