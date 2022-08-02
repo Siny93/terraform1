1e8706743f08ce53b941f0a25873a22960c596ec
Siny <sinydaniel93@gmail.com>
2022-08-02 17:46:03 -0400

    modules

---
+++ 04-modules/ec2/main.tf
@@ -0,0 +1,7 @@
+resource "aws_instance" "sample" {
+  ami                    = "ami-02cee382129c66e70"
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [var.sg_id]
+}
+
+variable "sg_id" {}
