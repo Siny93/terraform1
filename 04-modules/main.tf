02c0da540fe20f482c511bcefc7825f5bed04921
Siny <sinydaniel93@gmail.com>
2022-09-16 18:30:19 -0400

    modules

--- 04-modules/main.tf
+++ 04-modules/main.tf
@@ -1,20 +1,20 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
-terraform {
-  backend "s3" {
-    bucket = "terraform-93"
-    key    = "04-modules/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
-
-module "sg" {
-  source = "./sg"
-}
-
-module "ec2" {
-  source = "./ec2"
-  sg_id = module.sg.sg_id
-}
+//provider "aws" {
+//  region = "us-east-1"
+//}
+//
+//terraform {
+//  backend "s3" {
+//    bucket = "terraform-93"
+//    key    = "04-modules/terraform.tfstate"
+//    region = "us-east-1"
+//  }
+//}
+//
+//module "sg" {
+//  source = "./sg"
+//}
+//
+//module "ec2" {
+//  source = "./ec2"
+//  sg_id = module.sg.sg_id
+//}
